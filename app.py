a71aefbc436d2a4b02e25e8eb9843d5316f9ef16
Vivek Shaw <100988434+VivekShaw0801@users.noreply.github.com>
2024-06-18 04:29:40 +0000

    chnged

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 from streamlit_option_menu import option_menu
 
-st.write("Hello")
+st.write("  ")
 selected = option_menu(
     menu_title=None,
     options=["Home", "Prediction", "Feedback","Admin Login"],
